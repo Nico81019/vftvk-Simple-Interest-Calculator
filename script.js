cd59976c1398b5f548c27aa61a2f09b9361f0fb2
Nico81019 <nicotrimarche@gmail.com>
2022-01-03 15:16:46 +0000

    update

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ function validation(){
 
 function compute()
 {
+    let x = document.getElementById("principal").value;
+    if (x <= 0){
+        alert("Enter a positive number for Amount")
+       document.getElementById("principal").focus();
+    }
+    else {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value
@@ -19,7 +25,7 @@ function compute()
     document.getElementById("result").innerHTML =
     "If you deposit <mark> "+principal+"</mark>,\<br\>at an interest rate of <mark>"
     +rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
-    
+    }
 }
 function updateRate(){
     var rateval = document.getElementById("rate").value;
